b7860a8f976fb025056d6d3c9646db4108746dc9
wuch <wuch@shuizijie.com>
2023-08-04 13:56:11 +0800

    feat:增加lodash

--- src/components/test.ts
+++ src/components/test.ts
@@ -1,49 +1,48 @@
 import {
   MinPriorityQueue,
   PriorityQueueItem,
+  MaxPriorityQueue,
+  PriorityQueue,
 } from '@datastructures-js/priority-queue'
-export default function countCompleteComponents(
-  n: number,
-  edges: number[][],
-): number {
-  const edgesVertexUsed = new Set(edges.flat())
-  let count = n - edgesVertexUsed.size
-  const map = new Map<number, Set<number>>()
-  for (let i = 0; i < edges.length; i++) {
-    const edge = edges[i]
-    if (!map.has(edge[0])) {
-      map.set(edge[0], new Set([edge[1]]))
-    } else {
-      map.get(edge[0])?.add(edge[1])
-    }
-    if (!map.has(edge[1])) {
-      map.set(edge[1], new Set([edge[0]]))
-    } else {
-      map.get(edge[1])?.add(edge[0])
-    }
+import * as _ from 'lodash'
+export default function uniquePathsIII(grid: number[][]): number {
+  let gridTempStart = _.cloneDeep(grid)
+  let pathcount = 0
+  let start: number[]
+  const totalZero = grid.reduce(
+    (sum, arr, r) =>
+      sum +
+      arr.reduce((s, v, c) => {
+        if (v === 1) start = [r, c, 0]
+        return v === 0 ? s + 1 : s
+      }, 0),
+    0,
+  )
+  start![2] = totalZero
+  const stack = [start!]
+  const stackGrid = [gridTempStart]
+  while (stack.length > 0) {
+    const rawcolleft = stack.pop()
+    help(rawcolleft![0], rawcolleft![1], rawcolleft![2], stackGrid.pop()!)
   }
-  const searched = new Set<number>()
-  for (const [k, v] of map.entries()) {
-    let ss = 0
-    if (searched.has(k)) continue
-    const size = v.size
-    let condition = true
-    searched.add(k)
-    ss++
-    condition = condition && searchNode(v, size)
-    if (condition && ss === size) {
-      count++
-    }
-    function searchNode(set: Set<number>, size: number): boolean {
-      let condition = set.size === size
-      for (const t of set.values()) {
-        if (searched.has(t)) continue
-        searched.add(t)
-        ss++
-        condition = condition && searchNode(map.get(t)!, size)
+  return pathcount
+  function help(row: number, col: number, left: number, gridTemp: number[][]) {
+    move(row - 1, col)
+    move(row + 1, col)
+    move(row, col + 1)
+    move(row, col - 1)
+    function move(row: number, col: number) {
+      const mark = gridTemp[row]?.[col] as 0 | 1 | 2 | -1 | undefined
+      if (mark === 0) {
+        const temp = _.cloneDeep(gridTemp)
+        temp[row][col] = 1
+        stack.push([row, col, left - 1])
+        stackGrid.push(temp)
+        return
       }
-      return condition
+      if (mark === 2 && left === 0) {
+        pathcount++
+      }
     }
   }
-  return count
 }
